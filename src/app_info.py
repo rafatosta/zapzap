d80f425b2166cb59d9694f1805a6d187071420ed
Rafael Tosta Santos <rafa.ecomp@gmail.com>
2022-01-01 19:06:31 -0300

    novos ícones

--- src/app_info.py
+++ src/app_info.py
@@ -9,6 +9,6 @@ __url__ = 'https://web.whatsapp.com/'
 # Link para pegar o userAgent: http://httpbin.org/user-agent
 user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36"
 
-ICON = 'images/whatsapp.png'
+ICON = 'images/whatsapp.svg'
 
-ICON_MSG = 'images/withmsg.png'
+ICON_MSG = 'images/whatsapp_msg.svg'
