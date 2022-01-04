5c5d0ed373390093c37498dd5f19d9b492626caf
Rafael Tosta Santos <rafa.ecomp@gmail.com>
2022-01-03 21:42:15 -0300

    Abrindo links no navegador padrão do usuário (Sem sucesso)
    Investigar algo relacionado a página do whatsap.

--- src/whatsapp.py
+++ src/whatsapp.py
@@ -1,13 +1,29 @@
 from PySide6.QtCore import QUrl
+from PySide6.QtGui import QDesktopServices
 from PySide6.QtWebEngineCore import QWebEnginePage
-from app_info import user_agent
+from app_info import WHATS_URL, user_agent
 
 # Classe para a página do webapp.
+
+
 class WhatsApp(QWebEnginePage):
     def __init__(self, *args, **kwargs):
         QWebEnginePage.__init__(self, *args, **kwargs)
         self.featurePermissionRequested.connect(self.permission)
 
     def permission(self, frame, feature):
-        """Permissões para o navegador."""
-        self.setFeaturePermission(frame, feature, QWebEnginePage.PermissionGrantedByUser)
+        self.setFeaturePermission(
+            frame, feature, QWebEnginePage.PermissionGrantedByUser)
+
+    # Só funciona quando a página é carregada.
+    # No chat os links não são reconhecidos por essa função.
+    # Em caso de erro/falta de USER_AGENT, funciona nos link de downloads
+    def acceptNavigationRequest(self, url,  _type, isMainFrame):
+        print(url)
+        print(QWebEnginePage.NavigationTypeLinkClicked)
+        if (_type == QWebEnginePage.NavigationTypeLinkClicked and
+                url.host() != WHATS_URL):
+            # Send the URL to the system default URL handler.
+            QDesktopServices.openUrl(url)
+            return False
+        return super().acceptNavigationRequest(url,  _type, isMainFrame)
