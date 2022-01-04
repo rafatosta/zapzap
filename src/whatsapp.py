e486ce58dda59ca36e0b7308b03bceca2c5ba2e8
Rafael Tosta Santos <rafa.ecomp@gmail.com>
2022-01-04 14:25:48 -0300

    Abrindo links no navegador padrão do usuário

--- src/whatsapp.py
+++ src/whatsapp.py
@@ -1,29 +1,43 @@
-from PySide6.QtCore import QUrl
+from PySide6.QtCore import QEvent, Qt
 from PySide6.QtGui import QDesktopServices
 from PySide6.QtWebEngineCore import QWebEnginePage
+from PySide6.QtWidgets import QApplication
 from app_info import WHATS_URL, user_agent
 
 # Classe para a página do webapp.
 
 
 class WhatsApp(QWebEnginePage):
+
+    link_url = ''
+
     def __init__(self, *args, **kwargs):
         QWebEnginePage.__init__(self, *args, **kwargs)
+
+        # Ativa o EventFilter
+        QApplication.instance().installEventFilter(self)
+
+        # Acionar a resposta de permissão do recurso.
         self.featurePermissionRequested.connect(self.permission)
+
+        # Este sinal é emitido quando o mouse passa sobre um link
+        self.linkHovered.connect(self.link_hovered)
+
+    def link_hovered(self, url):
+        # url contém o URL de destino do link. Ao mover o mouse para fora da url o seu valor é definido como uma string vazia
+        self.link_url = url
 
     def permission(self, frame, feature):
         self.setFeaturePermission(
             frame, feature, QWebEnginePage.PermissionGrantedByUser)
 
-    # Só funciona quando a página é carregada.
-    # No chat os links não são reconhecidos por essa função.
-    # Em caso de erro/falta de USER_AGENT, funciona nos link de downloads
-    def acceptNavigationRequest(self, url,  _type, isMainFrame):
-        print(url)
-        print(QWebEnginePage.NavigationTypeLinkClicked)
-        if (_type == QWebEnginePage.NavigationTypeLinkClicked and
-                url.host() != WHATS_URL):
-            # Send the URL to the system default URL handler.
-            QDesktopServices.openUrl(url)
-            return False
-        return super().acceptNavigationRequest(url,  _type, isMainFrame)
+    # Abrindo links no navegador padrão do usuário
+    # Solução alternativa ao acceptNavigationRequest, pois não funcionou dentro do whatsapp.
+    # Mapeia os eventos do Mouse e abre o link a partir do capturado do signal linkHovered.
+    def eventFilter(self, obj, event):
+        if event.type() == QEvent.MouseButtonPress:
+            if event.button() == Qt.LeftButton:
+                if self.link_url != '' and self.link_url != WHATS_URL:
+                    QDesktopServices.openUrl(self.link_url)
+                    return True
+        return False
