b3a0c98666d1e488973e8666ccfd7911f4a9d4e2
Rafael Tosta Santos <rafa.ecomp@gmail.com>
2022-01-10 19:55:03 -0300

    Update UserAgent

--- src/app_info.py
+++ src/app_info.py
@@ -5,7 +5,7 @@ __version__ = '0.1'
 WHATS_URL = 'https://web.whatsapp.com/'
 
 # Link para pegar o userAgent: http://httpbin.org/user-agent
-user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36"
+user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36"
 
 ICON = ':/images/whatsapp.svg'
 
