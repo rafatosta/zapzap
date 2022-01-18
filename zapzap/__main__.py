d1a3a1816c2ebdc95f06aa4dbd8fa336f539f030
Rafael Tosta Santos <rafa.ecomp@gmail.com>
2022-01-18 20:31:03 -0300

    commit final
    último teste de construção

--- zapzap/__main__.py
+++ zapzap/__main__.py
@@ -1,5 +1,30 @@
+import sys
+import os
+from app_info import ICON, APPLICATION_NAME, __version__
+from PySide6.QtWidgets import QApplication
+from PySide6.QtGui import QIcon, QPixmap
+import resources_img
+
+from main_window import MainWindow
+
+
 def main():
-    print("APARECEU, ENTÃO FUNCIONOU!!")
+    # Verificando a plataforma onde o código está sendo executado.
+    if sys.platform == 'linux':
+        # Definindo o tipo de sessão onde o código será executado.
+        if os.getenv('XDG_SESSION_TYPE') == 'wayland':
+            # se deixar como wayland é aplicado a decoração da janela padrão do QT e não do sistema
+            os.environ['QT_QPA_PLATFORM'] = 'xcb'
+
+    app = QApplication(sys.argv)
+    app.setApplicationName(APPLICATION_NAME)
+    app.setApplicationVersion(__version__)
+    app.setWindowIcon(QIcon(QPixmap(ICON)))
+
+    window = MainWindow(app)
+    window.show()
+
+    sys.exit(app.exec())
 
 
 if __name__ == "__main__":
