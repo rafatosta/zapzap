fdc5bd94f4188664ff599a05ec4f15958149ec5b
Rafael Tosta Santos <rafa.ecomp@gmail.com>
2022-01-26 15:29:40 -0300

    Update userAgent
    Update userAgent

--- zapzap/app_info.py
+++ zapzap/app_info.py
@@ -5,4 +5,5 @@ __version__ = '1.0'
 WHATS_URL = 'https://web.whatsapp.com/'
 
 # Link para pegar o userAgent: http://httpbin.org/user-agent
-USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36"
+USER_AGENT = "Mozilla/5.0 (X11; Fedora; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36"
+                
