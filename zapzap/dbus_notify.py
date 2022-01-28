42ce081e0f3ae3f6a03690109322e69e0c3785f4
Rafael Tosta Santos <rafa.ecomp@gmail.com>
2022-01-27 21:09:35 -0300

    Create dbus_notify.py
    Notificação via dbus

---
+++ zapzap/dbus_notify.py
@@ -0,0 +1,20 @@
+import dbus
+from zapzap.app_info import APPLICATION_NAME
+
+
+def show_notify(q_notification):
+    item = "org.freedesktop.Notifications"
+    path = "/org/freedesktop/Notifications"
+    interface = "org.freedesktop.Notifications"
+    id_num_to_replace = 0
+    icon = 'com.rtosta.zapzap'
+    actions = {}
+    app_name = APPLICATION_NAME
+    hints = {}
+    time = 1000   # Use seconds x 1000
+    bus = dbus.SessionBus()
+    notif = bus.get_object(item, path)
+
+    notify = dbus.Interface(notif, interface)
+    notify.Notify(app_name, id_num_to_replace, icon,
+                  q_notification.title(), q_notification.message(), actions, hints, time)
