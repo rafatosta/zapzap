de172c5872ff78b051f542191ebb942cb14c4808
Rafael Tosta Santos <rafa.ecomp@gmail.com>
2022-01-28 18:44:12 -0300

    Update dbus_notify.py

--- zapzap/dbus_notify.py
+++ zapzap/dbus_notify.py
@@ -1,19 +1,17 @@
 import dbus
-import os
 
 from zapzap.app_info import APPLICATION_NAME
-
 
 def show(q_notification):
     item = "org.freedesktop.Notifications"
     path = "/org/freedesktop/Notifications"
     interface = "org.freedesktop.Notifications"
     id_num_to_replace = 0
-    icon = convertImage(q_notification.icon())
+    icon = 'com.rtosta.zapzap'
     actions = {}
     app_name = APPLICATION_NAME
     hints = {}
-    time = 1000   # Use seconds x 1000
+    time = 1000
     bus = dbus.SessionBus()
     notif = bus.get_object(item, path)
 
@@ -21,6 +19,9 @@ def show(q_notification):
     notify.Notify(app_name, id_num_to_replace, icon,
                   q_notification.title(), q_notification.message(), actions, hints, time)
 
+
+""" Não funciona, pois a url está dentro do flatpak.
+    Não faz sentido ficar escrevendo no disco toda vez que chegar uma notificação.
 def convertImage(img):
     ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
     print(ROOT_DIR)
@@ -34,4 +35,4 @@ def convertImage(img):
     if(confirm):
         return path
     else:
-        return 'com.rtosta.zapzap'
+        return 'com.rtosta.zapzap'"""
