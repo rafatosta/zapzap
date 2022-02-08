9d763e7b6c26e93315a38f9af1acba4f7867db65
Rafael Tosta Santos <rafa.ecomp@gmail.com>
2022-02-08 19:39:27 -0300

    Checkbox habilitados ou não

--- zapzap/controllers/settings.py
+++ zapzap/controllers/settings.py
@@ -1,4 +1,5 @@
 from PyQt6.QtWidgets import QWidget
+from PyQt6.QtCore import Qt
 from PyQt6 import uic
 
 
@@ -11,8 +12,23 @@ class Settings(QWidget):
 
         self.closeButton.clicked.connect(parent.onToggled)
 
-        # self.start_system
+        self.start_system.stateChanged.connect(self.state_start_system)
+
         self.night_mode.stateChanged.connect(self.state_night_mode)
+
+        self.notify_desktop.stateChanged.connect(self.state_notify_desktop)
 
     def state_night_mode(self, s):
         self.parent.parent.toggle_stylesheet()
+
+    def state_start_system(self, s):
+        self.start_hide.setEnabled(s)
+
+        print(s)
+
+    def state_notify_desktop(self, s):
+        self.show_photo.setEnabled(s)
+        self.show_name.setEnabled(s)
+        self.show_msg.setEnabled(s)
+
+        print(s)
