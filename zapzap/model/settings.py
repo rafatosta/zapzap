094c0ca12c0e308f3cbd5194caa6ffb16fbe8912
Rafael Tosta Santos <rafa.ecomp@gmail.com>
2022-02-09 11:52:33 -0300

    Update settings.py
    Arquivo JSON para armazenar as configurações do usuário

--- zapzap/model/settings.py
+++ zapzap/model/settings.py
@@ -1,13 +1,59 @@
-import zapzap.model.database as db
+from PyQt6.QtCore import QStandardPaths
+from zapzap import __appname__
+
+from os.path import expanduser, isdir
+from os import makedirs, remove
+from json import dump, load
+
+path_settings = QStandardPaths.writableLocation(
+    QStandardPaths.StandardLocation.ConfigLocation)
+
+j_folder = expanduser(path_settings+'/' + __appname__)
+j_file = j_folder + '/settings.json'
+
+default_js = {
+    "start_system": False,
+    "start_hide": False,
+    "night_mode": False,
+    "notify_desktop": True,
+    "show_photo": True,
+    "show_name": True,
+    "show_msg": True
+}
+
+settings_js = None
 
 
-def tableSettings() -> str:
-    return """
-            CREATE TABLE IF NOT EXISTS "Settings" (
-                "id"	INTEGER,
-                "name"	TEXT NOT NULL,
-                "start"	INTEGER DEFAULT 1,
-                "image" BLOB,
-                PRIMARY KEY("id" AUTOINCREMENT)
-            );
-            """
+def checkSettings():
+    try:
+        with open(j_file):
+            pass
+    except Exception as msg:
+        print('>>>>>> ', msg)
+        if not isdir(j_folder):
+            makedirs(j_folder)
+        with open(j_file, 'w') as jfile:
+            dump(default_js, jfile, indent=2)
+
+
+def get_setting(op):
+    if settings_js == None:
+        get_json()
+    return settings_js[op]
+
+
+def get_json():
+    global settings_js
+    with open(j_file) as jf:
+        settings_js = load(jf)
+
+
+def write_json(op, val):
+    with open(j_file, 'r') as jf:
+        objJson = load(jf)
+        objJson[op] = val
+
+    # Replace original file
+    remove(j_file)
+    with open(j_file, 'w') as jf:
+        dump(objJson, jf, indent=2)
