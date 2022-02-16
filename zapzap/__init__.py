1a6829ea83dc9feb77ce24a9ac143155601b6878
Rafael Tosta Santos <rafa.ecomp@gmail.com>
2022-02-16 20:09:07 -0300

    Update UserAgent

--- zapzap/__init__.py
+++ zapzap/__init__.py
@@ -12,7 +12,7 @@ __bugreport__ = 'https://github.com/rafatosta/zapzap/issues'
 
 __whatsapp_url__ = 'https://web.whatsapp.com/'
 # Link para pegar o userAgent: http://httpbin.org/user-agent
-__user_agent__ = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.80 Safari/537.36"
+__user_agent__ = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36"
 
 
 # iniciando os paths
