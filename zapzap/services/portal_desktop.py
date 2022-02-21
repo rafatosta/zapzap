bcdebac5e88664386bed54a6a17e79278231400d
Rafael Tosta Santos <rafa.ecomp@gmail.com>
2022-02-21 08:29:40 -0300

    Check local de execução
    Verifica o local da execução e determina o comando para inicialização automática

--- zapzap/services/portal_desktop.py
+++ zapzap/services/portal_desktop.py
@@ -1,5 +1,5 @@
 from zapzap import __version__, __appname__, __comment__
-from PyQt6.QtCore import QStandardPaths
+from PyQt6.QtCore import QStandardPaths, QFileInfo
 import os
 
 path_data = QStandardPaths.writableLocation(
@@ -7,18 +7,21 @@ path_data = QStandardPaths.writableLocation(
 
 
 def createDesktop():
+    # define a string de execução a depender do ambiente
+    exec = '/usr/bin/flatpak run --command=zapzap com.rtosta.zapzap' if QFileInfo(
+        __file__).absolutePath().startswith('/app/') else 'zapzap'
     conteudo = f"""[Desktop Entry]
-Name={__appname__}
-Comment= {__comment__}
-Version={__version__}
-Type=Application
-Exec=/usr/bin/flatpak run --command=zapzap com.rtosta.zapzap
-Icon=com.rtosta.zapzap
-Terminal=false
-Categories=Network;InstantMessaging;
-StartupNotify=false
-X-GNOME-Autostart-Delay=60
-X-Flatpak=com.rtosta.zapzap"""
+    Name={__appname__}
+    Comment= {__comment__}
+    Version={__version__}
+    Type=Application
+    Exec={exec}
+    Icon=com.rtosta.zapzap
+    Terminal=false
+    Categories=Network;InstantMessaging;
+    StartupNotify=false
+    X-GNOME-Autostart-Delay=60
+    X-Flatpak=com.rtosta.zapzap"""
 
     with open(path_data, 'w') as arquivo:
         arquivo.write(conteudo)
