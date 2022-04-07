84692d7d9ea788432cdfb58285d542e93532eee6
Bader Zaidan <bader.zaidan@rwth-aachen.de>
2022-04-07 17:17:57 +0200

    Use QtDBus instead of outdated dbus-python

--- zapzap/services/dbus_notify.py
+++ zapzap/services/dbus_notify.py
@@ -1,33 +1,42 @@
-import dbus
 from PyQt6.QtCore import QStandardPaths, Qt
 from PyQt6.QtGui import QPainter, QPainter, QImage, QBrush, QPen
+from PyQt6.QtDBus import QDBus, QDBusConnection, QDBusInterface
 from zapzap import __appname__
 from zapzap.services.portal_config import get_setting
 
 
 def show(q_notification):
-    item = "org.freedesktop.Notifications"
-    path = "/org/freedesktop/Notifications"
-    interface = "org.freedesktop.Notifications"
-    id_num_to_replace = 0
-    actions = {}
-    app_name = __appname__
-    hints = {}
-    time = 2000
-    bus = dbus.SessionBus()
-    notif = bus.get_object(item, path)
+        item = "org.freedesktop.Notifications"
+        path = "/org/freedesktop/Notifications"
+        interface = "org.freedesktop.Notifications"
+        id_num_to_replace = 0
+        actions = {}
+        app_name = __appname__
+        hints = {}
+        time = 2000 
+        bus = QDBusConnection.sessionBus()
+        notify = QDBusInterface(item, path, interface, bus)
 
-    # <expressao1> if <condicao> else <expressao2>
-    icon = getPathImage(q_notification.icon(), q_notification.title(
-    )) if get_setting('show_photo') else 'com.rtosta.zapzap'
+        icon = getPathImage(q_notification.icon(), q_notification.title(
+                )) if get_setting('show_photo') else 'com.rtosta.zapzap'
 
-    title = q_notification.title() if get_setting('show_name') else __appname__
+        title = q_notification.title() if get_setting('show_name') else __appname__
 
-    message = q_notification.message() if get_setting('show_msg') else 'New message...'
+        message = q_notification.message() if get_setting('show_msg') else 'New message...'
 
-    notify = dbus.Interface(notif, interface)
-    notify.Notify(app_name, id_num_to_replace, icon,
-                  title, message, actions, hints, time)
+        if not bus.isConnected():
+            print("Bus is not connected.")
+            return
+
+        if notify.isValid():
+            retval = notify.call(QDBus.CallMode.AutoDetect, "Notify", app_name,
+                            id_num_to_replace, icon, title, message,
+                            actions, hints, time)
+            if retval.errorName():
+                print("Failed to send notification: ", retval.errorMessage())
+        else:
+            print("Invalid interface.")
+
 
 
 # salva a imagem do contato na pasta de dados do app
