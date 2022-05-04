73b2bc9ac2ed55f20016a91cf6461be59e3c080c
Rafael Tosta Santos <rafa.ecomp@gmail.com>
2022-05-04 20:56:51 -0300

    remove old dark mode

--- zapzap/services/portal_theme.py
+++ zapzap/services/portal_theme.py
@@ -3,6 +3,5 @@ from PyQt6.QtWidgets import QApplication
 def isDarkTheme():
     app = QApplication.instance()
     current_style = app.style().objectName()
-    print(current_style, 'Dark'.upper() in current_style.upper() )
     return 'Dark'.upper() in current_style.upper()
 
