3064f0b2c2dd7200d7025d81370f4a6624bb7067
Rafael Tosta Santos <rafa.ecomp@gmail.com>
2022-05-20 21:03:31 -0300

    Base class for app dialogs

---
+++ zapzap/controllers/zapDialog.py
@@ -0,0 +1,17 @@
+from PyQt6.QtCore import Qt
+from PyQt6.QtWidgets import QDialog, QApplication
+from PyQt6 import uic
+
+
+class ZapDialog(QDialog):
+    def __init__(self, uic_path):
+        super().__init__()
+        self.setWindowFlags(Qt.WindowType.Tool)
+        uic.loadUi(uic_path, self)
+        self.centerPos()
+
+    def centerPos(self):
+        qrec = QApplication.instance().getWindow().geometry()
+        x = qrec.x() + (qrec.width() - self.width())/2
+        y = qrec.y() + (qrec.height() - self.height())/2
+        self.move(int(x), int(y))
