de604555d0930d01758093fff3d38d02264064c8
Rafael Tosta Santos <rafa.ecomp@gmail.com>
2022-05-22 10:46:45 -0300

    Close Event in NewUser

--- zapzap/controllers/new_user.py
+++ zapzap/controllers/new_user.py
@@ -7,3 +7,7 @@ from zapzap.controllers.zapDialog import ZapDialog
 class NewUser(ZapDialog):
     def __init__(self, parent=None):
         super().__init__(zapzap.abs_path+'/view/new_user.ui')
+
+
+        self.close_1.clicked.connect(self.close)
+        self.close_2.clicked.connect(self.close)
