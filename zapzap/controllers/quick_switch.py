c6bdd5c677ba84560d75ec74efbaf81c6ee426b4
Rafael Tosta Santos <rafa.ecomp@gmail.com>
2022-05-23 14:36:23 -0300

    Sets window decoration for QuickSwitch

--- zapzap/controllers/quick_switch.py
+++ zapzap/controllers/quick_switch.py
@@ -7,4 +7,11 @@ from zapzap.controllers.zapDialog import ZapDialog
 class QuickSwitch(ZapDialog):
     def __init__(self, parent=None):
         super().__init__(zapzap.abs_path+'/view/quick_switch.ui')
+        self.setWindowFlags(Qt.WindowType.Popup |
+                            Qt.WindowType.FramelessWindowHint)
+        radius = 8.0
+        path = QPainterPath()
+        path.addRoundedRect(QRectF(self.rect()), radius, radius)
+        mask = QRegion(path.toFillPolygon().toPolygon())
+        self.setMask(mask)
 
