7d085fea51dd0768c2f4a44418c452a5cc289d4f
Rafael Tosta Santos <rafa.ecomp@gmail.com>
2022-07-09 14:54:11 -0300

    QCHECHBOX and QRADIONBUTTON

--- zapzap/assets/themes/style.py
+++ zapzap/assets/themes/style.py
@@ -106,10 +106,212 @@ def buildTheme(p) -> str:
         }
     """
 
+    QCHECHBOX = """
+        QCheckBox
+        {
+            color: {windowText};
+            margin-bottom: 0.09em;
+            font: 11pt;
+        }
+
+        QCheckBox:disabled
+        {
+            color: {disabled};
+        }
+        QCheckBox::indicator
+        {
+            width: 24px;
+            height: 24px;
+        }
+        QCheckBox::indicator:unchecked,
+        QCheckBox::indicator:unchecked:focus
+        {
+            border-image: url({path}/checkbox_unchecked_disabled.svg);
+        }
+        QCheckBox::indicator:unchecked:hover,
+        QCheckBox::indicator:unchecked:pressed
+        {
+            border: none;
+            border-image: url({path}/checkbox_unchecked.svg);
+        }
+
+        QCheckBox::indicator:checked
+        {
+            border-image: url({path}/checkbox_checked.svg);
+        }
+
+        QCheckBox::indicator:checked:hover,
+        QCheckBox::indicator:checked:focus,
+        QCheckBox::indicator:checked:pressed
+        {
+            border: none;
+            border-image: url({path}/checkbox_checked.svg);
+        }
+
+        QCheckBox::indicator:indeterminate
+        {
+            border-image: url({path}/checkbox_indeterminate.svg);
+        }
+
+        QCheckBox::indicator:indeterminate:focus,
+        QCheckBox::indicator:indeterminate:hover,
+        QCheckBox::indicator:indeterminate:pressed
+        {
+            border-image: url({path}/checkbox_indeterminate.svg);
+        }
+
+        QCheckBox::indicator:indeterminate:disabled
+        {
+            border-image: url({path}/checkbox_indeterminate_disabled.svg);
+        }
+
+        QCheckBox::indicator:checked:disabled
+        {
+            border-image: url({path}/checkbox_checked_disabled.svg);
+        }
+
+        QCheckBox::indicator:unchecked:disabled
+        {
+            border-image: url({path}/checkbox_unchecked_disabled.svg);
+        }
+
+    """
+
+    QRADIONBUTTON = """
+        QRadioButton
+        {
+            spacing: 0.23em;
+            outline: none;
+            color: {windowText};
+            margin-bottom: 0.09em;
+        }
+        QRadioButton:disabled
+        {
+            color: {disabled};
+        }
+
+        QRadioButton::indicator
+        {
+            width: 150px;
+            height: 100px;
+        }
+
+        #rb_system::indicator:unchecked,
+        #rb_system::indicator:unchecked:focus,
+        #rb_system::indicator:unchecked:hover,
+        #rb_system::indicator:unchecked:pressed
+        {
+            border-image: url({path}/theme_system_unchecked.svg);
+        }
+
+        #rb_system::indicator:checked,
+        #rb_system::indicator:checked:hover,
+        #rb_system::indicator:checked:focus,
+        #rb_system::indicator:checked:pressed
+        {
+            border: none;
+            outline: none;
+            border-image: url({path}/theme_system_checked.svg);
+        }
+
+        #rb_light::indicator:unchecked,
+        #rb_light::indicator:unchecked:focus,
+        #rb_light::indicator:unchecked:hover,
+        #rb_light::indicator:unchecked:pressed
+        {
+            border-image: url({path}/theme_light_unchecked.svg);
+        }
+
+        #rb_light::indicator:checked,
+        #rb_light::indicator:checked:hover,
+        #rb_light::indicator:checked:focus,
+        #rb_light::indicator:checked:pressed
+        {
+            border: none;
+            outline: none;
+            border-image: url({path}/theme_light_checked.svg);
+        }
+
+        #rb_dark::indicator:checked,
+        #rb_dark::indicator:checked:hover,
+        #rb_dark::indicator:checked:focus,
+        #rb_dark::indicator:checked:pressed
+        {
+            border: none;
+            outline: none;
+            border-image: url({path}/theme_dark_checked.svg);
+        }
+
+        #rb_dark::indicator:unchecked,
+        #rb_dark::indicator:unchecked:focus,
+        #rb_dark::indicator:unchecked:hover,
+        #rb_dark::indicator:unchecked:pressed
+        {
+            border-image: url({path}/theme_dark_unchecked.svg);
+        }
+
+        #rb_tray_default::indicator:unchecked,
+        #rb_tray_default::indicator:unchecked:focus,
+        #rb_tray_default::indicator:unchecked:hover,
+        #rb_tray_default::indicator:unchecked:pressed
+        {
+            border-image: url({path}/tray_default_unchecked.svg);
+        }
+
+        #rb_tray_default::indicator:checked,
+        #rb_tray_default::indicator:checked:hover,
+        #rb_tray_default::indicator:checked:focus,
+        #rb_tray_default::indicator:checked:pressed
+        {
+            border: none;
+            outline: none;
+            border-image: url({path}/tray_default_checked.svg);
+        }
+
+        #rb_tray_light::indicator:unchecked,
+        #rb_tray_light::indicator:unchecked:focus,
+        #rb_tray_light::indicator:unchecked:hover,
+        #rb_tray_light::indicator:unchecked:pressed
+        {
+            border-image: url({path}/tray_light_unchecked.svg);
+        }
+
+        #rb_tray_light::indicator:checked,
+        #rb_tray_light::indicator:checked:hover,
+        #rb_tray_light::indicator:checked:focus,
+        #rb_tray_light::indicator:checked:pressed
+        {
+            border: none;
+            outline: none;
+            border-image: url({path}/tray_light_checked.svg);
+        }
+
+        #rb_tray_dark::indicator:unchecked,
+        #rb_tray_dark::indicator:unchecked:focus,
+        #rb_tray_dark::indicator:unchecked:hover,
+        #rb_tray_dark::indicator:unchecked:pressed
+        {
+            border-image: url({path}/tray_dark_unchecked.svg);
+        }
+
+        #rb_tray_dark::indicator:checked,
+        #rb_tray_dark::indicator:checked:hover,
+        #rb_tray_dark::indicator:checked:focus,
+        #rb_tray_dark::indicator:checked:pressed
+        {
+            border: none;
+            outline: none;
+            border-image: url({path}/tray_dark_checked.svg);
+        }
+    """
+
+
     STYLE_SHEET = f"""
         {QWIDGETS}
         {QMENU_BAR}
         {QMENU}
+        {QCHECHBOX}
+        {QRADIONBUTTON}
         """
 
     for chave, valor in p.getPallete().items():
