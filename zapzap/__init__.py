3a65a0b654671e6181002410733dadaa4ad0bde2
Rafael Tosta Santos <rafa.ecomp@gmail.com>
2022-07-10 17:13:36 -0300

    Remove old paths

--- zapzap/__init__.py
+++ zapzap/__init__.py
@@ -24,10 +24,6 @@ __user_agent__ = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, lik
 from PyQt6.QtCore import QFileInfo
 abs_path = QFileInfo(__file__).absolutePath()
 
-# Temas
-theme_light_path = abs_path+'/assets/stylesheets/light/stylesheet.qss'
-theme_dark_path = abs_path+'/assets/stylesheets/dark/stylesheet.qss'
-
 # Tray
 def getIconTray(theme = 'default', mode='normal'):
     return QIcon(abs_path+f'/assets/icons/tray/{theme}_{mode}.svg')
