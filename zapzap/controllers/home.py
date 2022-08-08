c9719af261905df5dbecc09a60b7d30d188ab6aa
Rafael Tosta <rafa.ecomp@gmail.com>
2022-08-08 15:54:19 -0300

    Move class UserContainer

--- zapzap/controllers/home.py
+++ zapzap/controllers/home.py
@@ -1,80 +1,10 @@
 import os
-from PyQt6.QtWidgets import QWidget, QPushButton, QApplication
-from PyQt6.QtCore import QSize
-import zapzap
-from zapzap.engine.browser import Browser
-from zapzap.model.user import UserDAO
-from zapzap.theme.builder_icon import getImageQIcon
-from zapzap.view.home import Ui_Home
 import shutil
-
-
-class UserContainer(QPushButton):
-
-    isSelected = False
-
-    styleSheet_normal = """
-    QPushButton {	
-       qproperty-iconSize: 20px;
-    }
-    """
-
-    styleSheet_hover = """
-    QPushButton {	
-       qproperty-iconSize: 30px;
-    }
-    """
-
-    def __init__(self, parent=None, user=None):
-        super(UserContainer, self).__init__()
-        self.user = user
-        self.home = parent
-
-        self.qtd = 0
-
-        self.setFlat(True)
-        self.setMinimumSize(QSize(30, 30))
-        self.setMaximumSize(QSize(30, 30))
-
-        self.browser = Browser(user.id, self)
-
-        self.setIcon(getImageQIcon(svg_str=user.icon))
-        self.setStyleSheet(self.styleSheet_normal)
-        self.clicked.connect(self.click)
-
-    def showIconNotification(self, qtd):
-        self.qtd = qtd
-        self.setIcon(getImageQIcon(svg_str=self.user.icon, qtd=qtd))
-        mainWindow = QApplication.instance().getWindow()
-        mainWindow.emitNotifications()
-
-    def click(self):
-        self.home.resetStyle()
-        self.home.setPage(self.browser)
-        self.selected()
-
-    def getBrowser(self):
-        return self.browser
-
-    def closeBrowser(self):
-        self.browser.stop()
-
-    ## EVENTS ##
-
-    def selected(self):
-        self.isSelected = True
-        self.setStyleSheet(self.styleSheet_hover)
-
-    def unselected(self):
-        self.isSelected = False
-        self.setStyleSheet(self.styleSheet_normal)
-
-    def enterEvent(self, e):
-        self.setStyleSheet(self.styleSheet_hover)
-
-    def leaveEvent(self, e):
-        if not self.isSelected:
-            self.setStyleSheet(self.styleSheet_normal)
+from PyQt6.QtWidgets import QWidget
+from zapzap.controllers.user_container import UserContainer
+from zapzap.model.user import UserDAO
+from zapzap.view.home import Ui_Home
+import zapzap
 
 
 class Home(QWidget, Ui_Home):
---
+++ zapzap/controllers/user_container.py
@@ -0,0 +1,72 @@
+from PyQt6.QtWidgets import QPushButton, QApplication
+from PyQt6.QtCore import QSize
+from zapzap.theme.builder_icon import getImageQIcon
+from zapzap.engine.browser import Browser
+
+
+class UserContainer(QPushButton):
+
+    isSelected = False
+
+    styleSheet_normal = """
+    QPushButton {	
+       qproperty-iconSize: 20px;
+    }
+    """
+
+    styleSheet_hover = """
+    QPushButton {	
+       qproperty-iconSize: 30px;
+    }
+    """
+
+    def __init__(self, parent=None, user=None):
+        super(UserContainer, self).__init__()
+        self.user = user
+        self.home = parent
+
+        self.qtd = 0
+
+        self.setFlat(True)
+        self.setMinimumSize(QSize(30, 30))
+        self.setMaximumSize(QSize(30, 30))
+
+        self.browser = Browser(user.id, self)
+
+        self.setIcon(getImageQIcon(svg_str=user.icon))
+        self.setStyleSheet(self.styleSheet_normal)
+        self.clicked.connect(self.click)
+
+    def showIconNotification(self, qtd):
+        self.qtd = qtd
+        self.setIcon(getImageQIcon(svg_str=self.user.icon, qtd=qtd))
+        mainWindow = QApplication.instance().getWindow()
+        mainWindow.emitNotifications()
+
+    def click(self):
+        self.home.resetStyle()
+        self.home.setPage(self.browser)
+        self.selected()
+
+    def getBrowser(self):
+        return self.browser
+
+    def closeBrowser(self):
+        self.browser.stop()
+
+    ## EVENTS ##
+
+    def selected(self):
+        self.isSelected = True
+        self.setStyleSheet(self.styleSheet_hover)
+
+    def unselected(self):
+        self.isSelected = False
+        self.setStyleSheet(self.styleSheet_normal)
+
+    def enterEvent(self, e):
+        self.setStyleSheet(self.styleSheet_hover)
+
+    def leaveEvent(self, e):
+        if not self.isSelected:
+            self.setStyleSheet(self.styleSheet_normal)
