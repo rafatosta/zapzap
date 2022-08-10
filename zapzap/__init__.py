a43efb3739dec3ac6e816db57e02e919c4db99c7
Rafael Tosta <rafa.ecomp@gmail.com>
2022-08-10 15:51:24 -0300

    updade user agent

--- zapzap/__init__.py
+++ zapzap/__init__.py
@@ -1,6 +1,6 @@
 import os
 from PyQt6.QtCore import QStandardPaths
-__version__ = '3.2'
+__version__ = '4.0'
 __appname__ = 'ZapZap'
 __comment__ = 'Whatsapp Desktop for linux'
 __domain__ = 'com.rtosta'
@@ -20,7 +20,7 @@ __licence__ = 'GNU General Public License v3.0'
 
 __whatsapp_url__ = 'https://web.whatsapp.com/'
 # Link para pegar o userAgent: http://httpbin.org/user-agent
-__user_agent__ = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Safari/537.36 Edg/103.0.1264.62"
+__user_agent__ = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.81 Safari/537.36 Edg/104.0.1293.47"
 
 # iniciando os paths
 from PyQt6.QtCore import QFileInfo
