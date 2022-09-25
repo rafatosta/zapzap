cfd7c323dc8c22d3aeaff2d9d0e6c8f2277b9056
Rafael Tosta <rafa.ecomp@gmail.com>
2022-09-25 15:46:39 -0300

    Update Desktop Entry

--- zapzap/services/portal_desktop.py
+++ zapzap/services/portal_desktop.py
@@ -1,9 +1,12 @@
-from zapzap import __version__, __appname__, __comment__
+from zapzap import __version__, __appname__, __comment__, isFlatpak
 from PyQt6.QtCore import QStandardPaths
 import os
 
 path_data = QStandardPaths.writableLocation(
     QStandardPaths.StandardLocation.ConfigLocation)+'/autostart/com.rtosta.zapzap.desktop'
+
+EXEC_FLATPAK = '/usr/bin/flatpak run com.rtosta.zapzap %u'
+EXEC_LOCAL = 'zapzap %u'
 
 
 def createDesktop(startHide):
@@ -12,18 +15,24 @@ def createDesktop(startHide):
     if startHide:
         flag = '--hideStart'
 
+    exec_ = EXEC_FLATPAK
+    if not isFlatpak:
+        exec_ = EXEC_LOCAL
+
     conteudo = f"""[Desktop Entry]
 Version=1.0
 Name=ZapZap
+Comment[pt_BR]=Whatsapp Desktop para Linux
 Comment=Whatsapp Desktop for Linux
-Exec=/usr/bin/flatpak run com.rtosta.zapzap {flag}
+Exec={exec_} {flag}
 Icon=com.rtosta.zapzap
 Type=Application
 Categories=Chat;Network;InstantMessaging;Qt;
-Keywords=whatsapp;chat;im;messaging;messenger;sms;
+Keywords=Whatsapp;Chat;ZapZap;
+StartupWMClass=zapzap
+MimeType=x-scheme-handler/whatsapp
 Terminal=false
 SingleMainWindow=true
-X-GNOME-Autostart-Delay=60
 X-GNOME-UsesNotifications=true
 X-GNOME-SingleWindow=true"""
 
