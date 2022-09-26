939e8e14ccd11735b8a149c9d7bacf13a41723f6
Rafael Tosta <rafa.ecomp@gmail.com>
2022-09-26 10:36:46 -0300

    Update run.py

--- run.py
+++ run.py
@@ -1,7 +1,14 @@
 import os
 
+# Build the windows from the .ui file
 os.system('./_scripts/build-windows.sh')
 
+# Build the translations
 os.system('./_scripts/build-translations.sh')
 
-os.system('python -m zapzap ')
+# Activate Custom Debug Settings
+debug = True
+
+# Run the app
+os.system('python -m zapzap ' + ('--zapDebug' if debug ==
+          True else ''))
