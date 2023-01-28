12c12996eb5cdfdce47eea001c4c9f77c102713a
Rafael Tosta <rafa.ecomp@gmail.com>
2023-01-28 12:06:31 -0300

    update userAgent

--- zapzap/__init__.py
+++ zapzap/__init__.py
@@ -22,7 +22,7 @@ __ddiHelper__ = 'https://www.ddi-ddd.com.br/Codigos-Telefone-Internacional/'
 
 __whatsapp_url__ = 'https://web.whatsapp.com/'
 # Link para pegar o userAgent: http://httpbin.org/user-agent
-__user_agent__ = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36"
+__user_agent__ = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36"
 
 # iniciando os paths
 from PyQt6.QtCore import QFileInfo
