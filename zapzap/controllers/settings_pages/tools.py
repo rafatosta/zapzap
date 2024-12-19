900ec4e20011b898a2f5e8d3e2d5004ba3911a9c
Rafael Tosta <rafa.ecomp@gmail.com>
2024-12-19 11:49:53 -0300

    initial rebase

--- zapzap/controllers/settings_pages/tools.py
+++
@@ -1,7 +0,0 @@
-from gettext import gettext as _
-
-def updateTextCheckBox(checkBox):
-        if checkBox.isChecked():
-            checkBox.setText(_("On"))
-        else:
-            checkBox.setText(_("Off"))
