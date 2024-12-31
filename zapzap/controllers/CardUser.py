62ea5c9f23e5e7d3e37cee5ed79956d5a5fc2af4
Rafael Tosta <rafa.ecomp@gmail.com>
2024-12-31 16:59:14 -0300

    Update CardUser.py

--- zapzap/controllers/CardUser.py
+++ zapzap/controllers/CardUser.py
@@ -1,7 +1,6 @@
 from PyQt6 import uic
 from PyQt6.QtWidgets import QWidget, QApplication
 
-
 from zapzap.models.User import User
 from zapzap.resources.UserIcon import UserIcon
 from zapzap.services.SettingsManager import SettingsManager
@@ -14,24 +13,25 @@ class CardUser(QWidget):
 
         self.user = user
 
+        # Configuração inicial da interface
         self._setup_ui()
         self._setup_signals()
         self._load_data()
         self._update_user_icon()
 
     def _setup_ui(self):
-        if self.user.id == User.USER_DEFAULT:  # user default
+        """Configura a interface do card de usuário."""
+        if self.user.id == User.USER_DEFAULT:  # Usuário padrão
             self.delete.hide()
 
     def _setup_signals(self):
-        """Configura os sinais para os elementos da interface."""
+        """Conecta os sinais aos métodos correspondentes."""
         self.silence.clicked.connect(self._handle_silence_action)
         self.disable.clicked.connect(self._handle_disable_action)
         self.delete.clicked.connect(self._handle_delete_action)
 
     def _load_data(self):
         """Carrega os dados do usuário na interface."""
-
         self.name.setText(self.user.name)
         self.disable.setChecked(not self.user.enable)
         self.silence.setChecked(
@@ -39,16 +39,16 @@ class CardUser(QWidget):
         )
 
     def _handle_disable_action(self):
-        """Ação para habilitar/desabilitar o usuário."""
+        """Habilita ou desabilita o usuário."""
         self.user.enable = not self.disable.isChecked()
         self._update_user_icon()
 
-        QApplication.instance().getWindow().browser.update_icons_page_button(self.user)
-        QApplication.instance().getWindow().browser.disable_page(self.user)
+        browser = QApplication.instance().getWindow().browser
+        browser.update_icons_page_button(self.user)
+        browser.disable_page(self.user)
 
     def _handle_silence_action(self):
-        """Ação para ativar/desativar notificações do usuário."""
-
+        """Ativa ou desativa as notificações do usuário."""
         SettingsManager.set(
             f"{self.user.id}/notification", not self.silence.isChecked()
         )
@@ -57,15 +57,19 @@ class CardUser(QWidget):
         QApplication.instance().getWindow().browser.update_icons_page_button(self.user)
 
     def _handle_delete_action(self):
-        """Ação para excluir o usuário."""
-        print("Usuário excluído!")  # Aqui você pode adicionar a lógica de exclusão.
+        """Exclui o usuário."""
+        print("Usuário excluído!")
+
+        # Informa ao Browser qual usuário será excluído
         QApplication.instance().getWindow().browser.delete_page(self.user)
 
+        # Remove o card da interface
         self.close()
 
     def _update_user_icon(self):
-        """Atualiza o ícone do usuário com base em seu status."""
+        """Atualiza o ícone do usuário com base no status."""
         user_icon_type = UserIcon.Type.Default
+
         if not self.user.enable:
             user_icon_type = UserIcon.Type.Disable
         elif not SettingsManager.get(f"{self.user.id}/notification", True):
