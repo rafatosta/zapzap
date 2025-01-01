fbcd91f5faf05a74a190b510dcbf06968139ead1
Rafael Tosta <rafa.ecomp@gmail.com>
2025-01-01 15:54:10 -0300

    Update CardUser.py

--- zapzap/controllers/CardUser.py
+++ zapzap/controllers/CardUser.py
@@ -1,6 +1,5 @@
 from PyQt6 import uic
 from PyQt6.QtWidgets import QWidget, QApplication
-
 from zapzap.models.User import User
 from zapzap.resources.UserIcon import UserIcon
 from zapzap.services.SettingsManager import SettingsManager
@@ -10,25 +9,28 @@ class CardUser(QWidget):
     def __init__(self, user: User = None, parent=None):
         super().__init__(parent)
         uic.loadUi("zapzap/ui/ui_card_user.ui", self)
-
         self.user = user
 
-        # Configuração inicial da interface
+        self._initialize()
+
+    def _initialize(self):
+        """Inicializa o card do usuário."""
         self._setup_ui()
         self._setup_signals()
         self._load_data()
         self._update_user_icon()
 
     def _setup_ui(self):
-        """Configura a interface do card de usuário."""
-        if self.user.id == User.USER_DEFAULT:  # Usuário padrão
+        """Configura a interface inicial do card de usuário."""
+        if self.user.id == User.USER_DEFAULT:  # Oculta exclusão para o usuário padrão
             self.delete.hide()
 
     def _setup_signals(self):
-        """Conecta os sinais aos métodos correspondentes."""
+        """Configura os sinais e suas respectivas ações."""
         self.silence.clicked.connect(self._handle_silence_action)
         self.disable.clicked.connect(self._handle_disable_action)
         self.delete.clicked.connect(self._handle_delete_action)
+        self.name.editingFinished.connect(self._update_user_name)
 
     def _load_data(self):
         """Carrega os dados do usuário na interface."""
@@ -37,6 +39,17 @@ class CardUser(QWidget):
         self.silence.setChecked(
             not SettingsManager.get(f"{self.user.id}/notification", True)
         )
+
+    def _update_user_icon(self):
+        """Atualiza o ícone do usuário com base no seu status."""
+        user_icon_type = UserIcon.Type.Default
+
+        if not self.user.enable:
+            user_icon_type = UserIcon.Type.Disable
+        elif not SettingsManager.get(f"{self.user.id}/notification", True):
+            user_icon_type = UserIcon.Type.Silence
+
+        self.icon.setIcon(UserIcon.get_icon(self.user.icon, user_icon_type))
 
     def _handle_disable_action(self):
         """Habilita ou desabilita o usuário."""
@@ -48,7 +61,7 @@ class CardUser(QWidget):
         browser.disable_page(self.user)
 
     def _handle_silence_action(self):
-        """Ativa ou desativa as notificações do usuário."""
+        """Ativa ou desativa notificações do usuário."""
         SettingsManager.set(
             f"{self.user.id}/notification", not self.silence.isChecked()
         )
@@ -60,19 +73,11 @@ class CardUser(QWidget):
         """Exclui o usuário."""
         print("Usuário excluído!")
 
-        # Informa ao Browser qual usuário será excluído
         QApplication.instance().getWindow().browser.delete_page(self.user)
 
-        # Remove o card da interface
         self.close()
 
-    def _update_user_icon(self):
-        """Atualiza o ícone do usuário com base no status."""
-        user_icon_type = UserIcon.Type.Default
-
-        if not self.user.enable:
-            user_icon_type = UserIcon.Type.Disable
-        elif not SettingsManager.get(f"{self.user.id}/notification", True):
-            user_icon_type = UserIcon.Type.Silence
-
-        self.icon.setIcon(UserIcon.get_icon(self.user.icon, user_icon_type))
+    def _update_user_name(self):
+        """Atualiza o nome do usuário no banco de dados."""
+        self.user.name = self.name.text()
+        QApplication.instance().getWindow().browser.update_icons_page_button(self.user)
