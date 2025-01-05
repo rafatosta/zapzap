70e3bb41e88e185facf5b8bbe34abf5a2151b146
Rafael Tosta <rafa.ecomp@gmail.com>
2025-01-05 19:11:23 -0300

    Update DictionariesManager.py

--- zapzap/services/DictionariesManager.py
+++ zapzap/services/DictionariesManager.py
@@ -1,19 +1,31 @@
 import os
 from PyQt6.QtCore import QLocale
-
 from zapzap.config.SetupManager import SetupManager
 from zapzap.services.SettingsManager import SettingsManager
 
 
 class DictionariesManager:
-    """Gerencia os dicionários """
+    """Gerencia os dicionários de linguagem do sistema."""
 
     QTWEBENGINE_DICTIONARIES_PATH = "/usr/share/qt6/qtwebengine_dictionaries"
-
     QTWEBENGINE_DICTIONARIES_PATH_FLATPAK = "/run/host/usr/share/qt6/qtwebengine_dictionaries"
 
     @staticmethod
+    def get_path() -> str:
+        """
+        Retorna o caminho para os dicionários, dependendo do ambiente de execução.
+        """
+        return (
+            DictionariesManager.QTWEBENGINE_DICTIONARIES_PATH_FLATPAK
+            if SetupManager._is_flatpak
+            else DictionariesManager.QTWEBENGINE_DICTIONARIES_PATH
+        )
+
+    @staticmethod
     def list_files():
+        """
+        Exibe os idiomas disponíveis no console.
+        """
         dictionaries_path = DictionariesManager.get_path()
         if dictionaries_path and os.path.isdir(dictionaries_path):
             print("Linguagens disponíveis:")
@@ -24,33 +36,41 @@ class DictionariesManager:
             print("Caminho não encontrado ou inválido.")
 
     @staticmethod
-    def get_path():
-        if SetupManager._is_flatpak:
-            return DictionariesManager.QTWEBENGINE_DICTIONARIES_PATH_FLATPAK
-
-        return DictionariesManager.QTWEBENGINE_DICTIONARIES_PATH
-
-    @staticmethod
     def list() -> list:
+        """
+        Retorna uma lista de idiomas disponíveis.
+        """
         dict_list = []
         dictionaries_path = DictionariesManager.get_path()
         if dictionaries_path and os.path.isdir(dictionaries_path):
-            for file in os.listdir(dictionaries_path):
-                if file.endswith(".bdic"):
-                    dict_list.append(file.replace(".bdic", ""))
+            dict_list = [
+                file.replace(".bdic", "")
+                for file in os.listdir(dictionaries_path)
+                if file.endswith(".bdic")
+            ]
         else:
             print("Caminho não encontrado ou inválido.")
-
         return dict_list
 
     @staticmethod
-    def set_lang(lang):
+    def set_lang(lang: str):
+        """
+        Define o idioma atual para o corretor ortográfico.
+        """
         SettingsManager.set("system/spellCheckLanguage", lang)
-        # Avisa para o browser
 
     @staticmethod
-    def get_current_dict():
-        return SettingsManager.get("system/spellCheckLanguage", DictionariesManager.get_system_language())
+    def get_current_dict() -> str:
+        """
+        Retorna o idioma atualmente configurado para o corretor ortográfico.
+        """
+        return SettingsManager.get(
+            "system/spellCheckLanguage", DictionariesManager.get_system_language()
+        )
 
-    def get_system_language():
+    @staticmethod
+    def get_system_language() -> str:
+        """
+        Retorna o idioma padrão do sistema.
+        """
         return QLocale.system().name()
