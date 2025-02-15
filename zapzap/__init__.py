5abd8d7c38172c4a238bfcc4a0b143d9a7044c59
Rafael Tosta <rafa.ecomp@gmail.com>
2025-02-15 07:27:45 -0300

    Update __init__.py

--- zapzap/__init__.py
+++ zapzap/__init__.py
@@ -20,7 +20,7 @@ __donationPage__ = 'https://rtosta.com/zapzap-web/#donate'
 
 __whatsapp_url__ = 'https://web.whatsapp.com/'
 # Link para pegar o userAgent: http://httpbin.org/user-agent
-__user_agent__ = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36"
+__user_agent__ = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36"
 
 LIMITE_USERS = 3
 
