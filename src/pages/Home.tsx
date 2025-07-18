06aa99bf212eb2fb636b150aea20dd82dcece0b0
Rafael Tosta <rafa.ecomp@gmail.com>
2025-07-18 10:01:56 -0300

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -44,60 +44,61 @@ function Home() {
             ),
         }
     ];
-    return (<div className="relative flex w-full max-w-5xl flex-col items-center justify-center gap-12">
-        <div className="relative flex flex-col items-center gap-6">
-            <h1 className="relative text-center text-6xl leading-[125%] font-bold text-gray-900 dark:text-gray-200">
-                ZapZap
-            </h1>
-            <span className="inline-flex flex-wrap items-center justify-center gap-2.5 text-center">
-                <span className="inline text-xl text-gray-600 dark:text-gray-400">
-                    A Linux WhatsApp web app with a native application experience.
+    return (
+        <div className="relative flex w-full max-w-5xl h-screen flex-col items-center justify-center gap-12">
+            <div className="relative flex flex-col items-center gap-6">
+                <h1 className="relative text-center text-6xl leading-[125%] text-shadow-lg/30 font-bold text-gray-900 dark:text-gray-200">
+                    ZapZap
+                </h1>
+                <span className="inline-flex flex-wrap items-center justify-center gap-2.5 text-center">
+                    <span className="inline text-xl text-shadow-lg text-gray-600 dark:text-gray-400">
+                        A Linux WhatsApp web app with a native application experience.
+                    </span>
                 </span>
-            </span>
-        </div>
-
-        {/* Mostra última versão disponível (Coletar do github) */}
-        <div className='flex justify-center items-center gap-4 text-gray-600 dark:text-gray-400'>
-            <span>{"Last releases"}</span>
-            <div className="flex justify-center items-center gap-2">
-                <FaLinux className="w-6 h-6" />
-                <span>{"xxx..xxx...x"}</span>
             </div>
 
-        </div>
+            {/* Mostra última versão disponível (Coletar do github) */}
+            <div className='flex justify-center items-center gap-4 text-gray-600 dark:text-gray-400'>
+                <span>{"Last releases"}</span>
+                <div className="flex justify-center items-center gap-2">
+                    <FaLinux className="w-6 h-6" />
+                    <span>{"xxx..xxx...x"}</span>
+                </div>
 
-        {/* Versões disponíveis */}
-        <div className="relative flex w-full flex-col items-start gap-6 self-stretch">
-            <div className="grid w-full grid-cols-1 gap-6 md:grid-cols-2">
-                {CARDS.map((card) => (
-                    <a
-                        key={card.title}
-                        href={card.url}
-                        target="_blank"
-                        className="outline-primary-600 dark:outline-primary-500 group hover:border-primary-600 dark:hover:border-primary-500 cursor-pointer overflow-hidden rounded-xl border border-gray-200 bg-gray-50 outline-offset-2 focus:outline-2 dark:border-gray-700 dark:bg-gray-800"
-                    >
-                        <div className="flex items-center gap-6 p-4 jus h-full">
-                            <div className="flex flex-1 items-center gap-4">
-                                <div className="size-9">{card.icon}</div>
+            </div>
 
-                                <div className="flex flex-1 flex-col items-start justify-center gap-1.5 border-l border-gray-200 pl-3.5 dark:border-gray-700">
-                                    <div className="w-full font-sans text-lg leading-4 font-semibold text-gray-900 dark:text-gray-200">
-                                        {card.title}
-                                    </div>
+            {/* Versões disponíveis */}
+            <div className="relative flex w-full flex-col items-start gap-6 self-stretch">
+                <div className="grid w-full grid-cols-1 gap-6 md:grid-cols-2">
+                    {CARDS.map((card) => (
+                        <a
+                            key={card.title}
+                            href={card.url}
+                            target="_blank"
+                            className="outline-primary-600 dark:outline-primary-500 group hover:border-primary-600 dark:hover:border-primary-500 cursor-pointer overflow-hidden rounded-xl border border-gray-200 bg-gray-50 outline-offset-2 focus:outline-2 dark:border-gray-700 dark:bg-gray-800"
+                        >
+                            <div className="flex items-center gap-6 p-4 jus h-full">
+                                <div className="flex flex-1 items-center gap-4">
+                                    <div className="size-9">{card.icon}</div>
 
-                                    <div className="w-full font-sans text-sm leading-5 font-normal text-gray-500 dark:text-gray-400">
-                                        {card.description}
+                                    <div className="flex flex-1 flex-col items-start justify-center gap-1.5 border-l border-gray-200 pl-3.5 dark:border-gray-700">
+                                        <div className="w-full font-sans text-lg leading-4 font-semibold text-gray-900 dark:text-gray-200">
+                                            {card.title}
+                                        </div>
+
+                                        <div className="w-full font-sans text-sm leading-5 font-normal text-gray-500 dark:text-gray-400">
+                                            {card.description}
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
 
-                            <HiChevronRight className="text-gray-500 dark:text-gray-400" />
-                        </div>
-                    </a>
-                ))}
+                                <HiChevronRight className="text-gray-500 dark:text-gray-400" />
+                            </div>
+                        </a>
+                    ))}
+                </div>
             </div>
-        </div>
-    </div>);
+        </div>);
 }
 
 export default Home;
