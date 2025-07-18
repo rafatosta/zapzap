47578b2e4859621075265b84dfcaa9ae61cab553
Rafael Tosta <rafa.ecomp@gmail.com>
2025-07-18 10:02:01 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,28 @@
-import { DarkThemeToggle } from "flowbite-react";
 
-import patternLight from "/pattern-light.svg"
-import patternDark from "/pattern-dark.svg"
 import Home from "./pages/Home";
+import Features from "./pages/Features";
+import Background from "./components/Background";
+import WhyZapZap from "./pages/WhyZapZap";
 
 
 
 export default function App() {
-  
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-white px-4 py-24 dark:bg-gray-900">
-      {/* Mudança de tema */}
-      <div className="absolute inset-0 size-full">
-        <div className="relative h-full w-full select-none">
-          <img
-            className="absolute right-0 min-w-dvh dark:hidden"
-            alt="Pattern Light"
-            src={patternLight}
-          />
-          <img
-            className="absolute right-0 hidden min-w-dvh dark:block"
-            alt="Pattern Dark"
-            src={patternDark}
-          />
-        </div>
-      </div>
-      <div className="absolute top-4 right-4">
-        <DarkThemeToggle />
-      </div>
-      {/* Home */}
 
+  return (
+    <main className="
+        flex min-h-screen flex-col items-center justify-center 
+      bg-white px-4 dark:bg-gray-900 
+      text-gray-900 dark:text-gray-200
+      "
+    >
+      {/*Background geral */}
+      <Background />
+
+      {/* Páginas */}
       <Home />
-      
+      <Features />
+      <WhyZapZap />
+
     </main>
   );
 }
