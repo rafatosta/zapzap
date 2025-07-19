26641a0a885cc02cdea7adaee454d6d0cde79ba0
Rafael Tosta <rafa.ecomp@gmail.com>
2025-07-19 16:01:29 -0300

    Update Background.tsx

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -1,31 +1,46 @@
 import { DarkThemeToggle } from "flowbite-react";
 
-import patternLight from "/pattern-light.svg"
-import patternDark from "/pattern-dark.svg"
-
+/* import patternLight from "/pattern-light.svg";
+import patternDark from "/pattern-dark.svg"; */
+import zapzapLogo from "/zapzap.svg";
 
 function Background() {
-    return (
-        <>
-            <div className="fixed inset-0 size-full">
-                <div className="relative h-full w-full select-none">
-                    <img
-                        className="absolute right-0 min-w-dvh dark:hidden"
-                        alt="Pattern Light"
-                        src={patternLight}
-                    />
-                    <img
-                        className="absolute right-0 hidden min-w-dvh dark:block"
-                        alt="Pattern Dark"
-                        src={patternDark}
-                    />
-                </div>
-            </div>
-            <div className="fixed top-4 right-4">
-                <DarkThemeToggle />
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className="fixed inset-0 size-full">
+        <div className="relative h-full w-full select-none">
+          {/* Background */}
+          {/* <img
+            className="absolute right-0 min-w-dvh dark:hidden"
+            alt="Pattern Light"
+            src={patternLight}
+          />
+          <img
+            className="absolute right-0 hidden min-w-dvh dark:block"
+            alt="Pattern Dark"
+            src={patternDark}
+          /> */}
+
+          {/* Central */}
+          <div
+            className="absolute inset-0 flex items-center justify-center opacity-5 pointer-events-none"
+            aria-hidden="true"
+          >
+            <img
+              src={zapzapLogo}
+              alt="Zapzap Watermark"
+              className="w-3/4 max-w-md"
+            />
+          </div>
+        </div>
+      </div>
+
+      {/* Theme Toggle */}
+      <div className="fixed top-4 right-4 z-50">
+        <DarkThemeToggle />
+      </div>
+    </>
+  );
 }
 
 export default Background;
