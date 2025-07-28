6ef762c523d9c413ba484760e933145711b60a81
Rafael Tosta <rafa.ecomp@gmail.com>
2025-07-28 17:46:18 -0300

    iniital electron project

---
+++ src/renderer/src/main.tsx
@@ -0,0 +1,11 @@
+import './assets/main.css'
+
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+)
