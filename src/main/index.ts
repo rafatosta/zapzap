c800f639c774c87edf7a3c50127474dd8b20a548
Rafael Tosta <rafa.ecomp@gmail.com>
2025-07-29 13:19:28 -0300

    Update application information

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,7 +1,7 @@
 import { app, shell, BrowserWindow, ipcMain } from 'electron'
 import { join } from 'path'
 import { electronApp, optimizer, is } from '@electron-toolkit/utils'
-import icon from '../../resources/icon.png?asset'
+import icon from '../../resources/icon.svg?asset'
 
 function createWindow(): void {
   // Create the browser window.
