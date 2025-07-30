6fe7a64b6e3e2e36fc48649988006d08a3d71768
Rafael Tosta <rafa.ecomp@gmail.com>
2025-07-30 09:11:45 -0300

    update useragent

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,5 +1,5 @@
 
-const whatsappUserAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36';
+const whatsappUserAgent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36"
 const whatsappURL = "https://web.whatsapp.com"
 
 function App(): React.JSX.Element {
