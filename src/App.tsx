50bc6c34763a90094a91962fa287ca49bb2a5870
Rafael Tosta <rafa.ecomp@gmail.com>
2025-08-03 18:09:59 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,29 +5,37 @@ import Background from "./components/Background";
 import WhyZapZap from "./pages/WhyZapZap";
 import About from "./pages/About";
 import Donate from "./pages/Donate";
+import { Navbar } from "./components/Navbar";
+import Download from "./pages/Download";
 
 
 
 export default function App() {
 
   return (
-    <main className="
+
+    <>
+      <Navbar />
+      <main className="
         flex flex-col items-center justify-center min-h-screen
       bg-white px-4 dark:bg-gray-900 
       text-gray-900 dark:text-gray-100
       gap-20
       "
-    >
-      {/*Background geral */}
-      <Background />
+      >
+        {/*Background geral */}
+        <Background />
 
-      {/* Páginas */}
-      <Home />
-      <Features />
-      <WhyZapZap />
-      <Donate />
-      <About />
+        {/* Páginas */}
+        <Home />
+        <Features />
+        <WhyZapZap />
+        <Download />
+        <Donate />
+        <About />
 
-    </main>
+      </main>
+    </>
+
   );
 }
