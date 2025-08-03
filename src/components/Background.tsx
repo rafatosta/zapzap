e848dc559428611c1c8fea01486383a231b19b61
Rafael Tosta <rafa.ecomp@gmail.com>
2025-08-03 18:09:54 -0300

    Update Background.tsx

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -36,7 +36,7 @@ function Background() {
       </div>
 
       {/* Theme Toggle */}
-      <div className="fixed top-4 right-4 z-50">
+      <div className="fixed top-22 right-4 z-50">
         <DarkThemeToggle />
       </div>
     </>
