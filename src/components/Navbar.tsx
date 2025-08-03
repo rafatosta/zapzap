727ca26097d8c6747988a5e4f9ff42fcc756f04e
Rafael Tosta <rafa.ecomp@gmail.com>
2025-08-03 18:17:40 -0300

    Create Navbar.tsx

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,91 @@
+
+import {
+  Avatar,
+  Dropdown,
+  DropdownDivider,
+  DropdownHeader,
+  DropdownItem,
+  Navbar as NavbarFlowbit,
+  NavbarBrand,
+  NavbarCollapse,
+  NavbarLink,
+  NavbarToggle,
+} from "flowbite-react";
+
+import zapzapLogo from "/zapzap.svg";
+import { useEffect } from "react";
+import { scroller, Link } from 'react-scroll';
+
+export function Navbar() {
+
+  const navigationLinks = [
+    { name: "Home", href: "home" },
+    { name: "Features", href: "features" },
+    { name: "Why ZapZap", href: "why" },
+    { name: "Download", href: "download" },
+    { name: "Donate", href: "donate" },
+    { name: "About", href: "about" },
+  ];
+
+  useEffect(() => {
+    if (window.location.hash == "#donate") {
+      scroller.scrollTo('#donate', {
+        duration: 500,
+        smooth: true,
+      })
+    }
+  }, [])
+
+
+  return (
+    <NavbarFlowbit fluid rounded className="fixed w-full z-50  text-gray-900 dark:text-gray-100">
+      <NavbarBrand href="/">
+        <img src={zapzapLogo} className="mr-3 h-6 sm:h-9" alt="ZapZap Logo" />
+        <span className="self-center whitespace-nowrap text-xl font-semibold">ZapZap</span>
+      </NavbarBrand>
+      <div className="flex md:order-2">
+        <Dropdown
+          arrowIcon={false}
+          inline
+          label={
+            <Avatar alt="User settings" img="https://flowbite.com/docs/images/people/profile-picture-5.jpg" rounded />
+          }
+        >
+          <DropdownHeader>
+            <span className="block text-sm">Bonnie Green</span>
+            <span className="block truncate text-sm font-medium">name@flowbite.com</span>
+          </DropdownHeader>
+          <DropdownItem>Dashboard</DropdownItem>
+          <DropdownItem>Settings</DropdownItem>
+          <DropdownItem>Earnings</DropdownItem>
+          <DropdownDivider />
+          <DropdownItem>Sign out</DropdownItem>
+        </Dropdown>
+        <NavbarToggle />
+      </div>
+      <NavbarCollapse>
+        {navigationLinks.map((link) => (
+          <NavbarLink>
+            <Link key={link.name}
+              className="text-gray-700 dark:text-gray-100 cursor-pointer"
+              to={link.href}
+              activeClass="active"
+              spy={true}
+              hashSpy={true}
+              smooth={true}
+              duration={500}
+              activeStyle={{
+                color: '#20ba5b',
+                fontWeight: 'bold'
+              }}
+
+            >
+              {link.name}
+            </Link>
+          </NavbarLink>
+
+        ))}
+      </NavbarCollapse>
+    </NavbarFlowbit>
+  );
+}
