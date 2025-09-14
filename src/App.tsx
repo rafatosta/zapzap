8e6556bdd19ac7e5038c6338321ef4ff121403ce
rafatosta <rafa.ecomp@gmail.com>
2025-09-14 17:52:46 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import AboutSection from "./components/AboutSection";
 import DownloadSection from "./components/DownloadSection";
 import FeatureSection from "./components/FeatureSection";
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 
@@ -16,6 +17,7 @@ export default function App() {
       <FeatureSection />
       <DownloadSection />
       <AboutSection />
+      <Footer />
     </div>
   );
 }
