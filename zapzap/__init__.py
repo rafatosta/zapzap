d9815ad9ee8c9ff8fdfdc733f676ed90caeeef7a
Rafael Tosta <rafa.ecomp@gmail.com>
2026-01-21 20:36:30 -0300

    Update translations and bump version to 6.2.8
    Updated translation files to move the 'New message...' string to a new line reference, updated POT-Creation-Date, and incremented the app version to 6.2.8. Also updated the user agent string and added a new release entry in the appdata XML for the clipboard logic fix.

--- zapzap/__init__.py
+++ zapzap/__init__.py
@@ -1,5 +1,5 @@
 from PyQt6.QtCore import QFileInfo
-__version__ = '6.2.7'
+__version__ = '6.2.8'
 __appname__ = 'ZapZap'
 __comment__ = 'WhatsApp Messenger for linux'
 __domain__ = 'com.rtosta'
@@ -21,7 +21,7 @@ __donationPage__ = 'https://rtosta.com/zapzap/#donate'
 
 __whatsapp_url__ = 'https://web.whatsapp.com/'
 # Link para pegar o userAgent: http://httpbin.org/user-agent
-__user_agent__ = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36"
+__user_agent__ = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.0.0 Safari/537.36"
 
 LIMITE_USERS = 3
 
