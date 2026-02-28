5b45fc9296df9f65de628771934edc0ee10d17d6
Rafael Tosta <rafa.ecomp@gmail.com>
2026-02-28 11:26:13 -0300

    fix #585

--- zapzap/services/DownloadManager.py
+++ zapzap/services/DownloadManager.py
@@ -1,15 +1,22 @@
 from PyQt6.QtWebEngineCore import QWebEngineDownloadRequest
 from PyQt6.QtCore import QStandardPaths
 from zapzap.services.SettingsManager import SettingsManager
-from PyQt6.QtCore import Qt, QPoint
+from PyQt6.QtWidgets import QFileDialog
 
 from zapzap.controllers.DownloadToaster import DownloadToaster
+from gettext import gettext as _
 
 
 class DownloadManager:
     DOWNLOAD_PATH = QStandardPaths.writableLocation(
         QStandardPaths.StandardLocation.DownloadLocation
     )
+
+    _floating_cards = []
+
+    @staticmethod
+    def set_path(new_path):
+        SettingsManager.set("system/download_path", new_path)
 
     @staticmethod
     def get_path():
@@ -18,7 +25,12 @@ class DownloadManager:
             DownloadManager.DOWNLOAD_PATH
         )
 
-    _floating_cards = []
+    @staticmethod
+    def restore_path():
+        SettingsManager.set(
+            "system/download_path",
+            DownloadManager.DOWNLOAD_PATH
+        )
 
     @staticmethod
     def on_downloadRequested(
@@ -37,3 +49,22 @@ class DownloadManager:
 
         toaster = DownloadToaster(download, parent)
         toaster.show()
+
+    @staticmethod
+    def open_folder_dialog(parent):
+        directory = DownloadManager.get_path()
+
+        options = (
+            QFileDialog.Option.DontUseNativeDialog
+            if SettingsManager.get("system/DontUseNativeDialog", False)
+            else QFileDialog.Option(0)
+        )
+
+        folder_path = QFileDialog.getExistingDirectory(
+            parent=parent,
+            caption=_("Select folder"),
+            directory=directory,
+            options=options
+        )
+
+        return folder_path or None
