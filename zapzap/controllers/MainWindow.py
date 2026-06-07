799ae8dc09c5e7e72dd3aae514a98b7a71ed488e
Rafael Tosta <rafa.ecomp@gmail.com>
2026-06-07 16:17:14 -0300

    Update MainWindow.py

--- zapzap/controllers/MainWindow.py
+++ zapzap/controllers/MainWindow.py
@@ -25,7 +25,6 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.setupUi(self)
 
         self.was_maximized = False
-        self.normal_geometry = None
 
         self.is_fullscreen = False  # Controle do estado de tela cheia
         self.browser = Browser(self)  # Inicialização do navegador
@@ -279,8 +278,6 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         """Preparar o app para permanecer em segundo plano."""
 
         self.was_maximized = self.isMaximized()
-        if not self.was_maximized:
-            self.normal_geometry = self.saveGeometry()
 
         # Fecha o painel de configurações se estiver aberto
         if self.app_settings:
@@ -297,14 +294,17 @@ class MainWindow(QMainWindow, Ui_MainWindow):
             return
         """Alterna a visibilidade da janela principal."""
         if self.isHidden():
+            self.show()
+
             if self.is_fullscreen:
                 self.showFullScreen()
+
             elif self.was_maximized:
-                self.showMaximized()
-            else:
-                self.showNormal()
-                if self.normal_geometry:
-                    self.restoreGeometry(self.normal_geometry)
+                QTimer.singleShot(0, self.showMaximized)
+
+            self.activateWindow()
+            self.raise_()
+
             QApplication.instance().setActiveWindow(self)
         elif not self.isActiveWindow():
             self.activateWindow()
