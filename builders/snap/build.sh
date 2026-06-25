38d6236b09113854c46d0efc282e8ccad0417c3b
Rafael Tosta <rafa.ecomp@gmail.com>
2026-06-24 21:02:53 -0300

    Fix Snapcraft project source path

--- builders/snap/build.sh
+++ builders/snap/build.sh
@@ -3,6 +3,19 @@
 set -eu
 
 SCRIPT_DIR=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
-cd "${SCRIPT_DIR}"
+ROOT_DIR=$(CDPATH= cd -- "${SCRIPT_DIR}/../.." && pwd)
+PROJECT_DIR=${SNAPCRAFT_PROJECT_DIR:-"${ROOT_DIR}/.snapcraft-project"}
 
-snapcraft "$@"
+rm -rf "${PROJECT_DIR}"
+mkdir -p "${PROJECT_DIR}"
+
+rsync -a \
+    --exclude .git \
+    --exclude .snapcraft-project \
+    "${ROOT_DIR}/" \
+    "${PROJECT_DIR}/"
+
+cp "${SCRIPT_DIR}/snapcraft.yaml" "${PROJECT_DIR}/snapcraft.yaml"
+
+cd "${PROJECT_DIR}"
+snapcraft pack "$@"
