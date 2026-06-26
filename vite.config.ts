1b6c0b7200193ebb8a09ce25a3b6eac22210c7b3
Rafael Tosta <rafa.ecomp@gmail.com>
2026-06-26 19:59:34 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import path from "node:path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
+  base: '/zapzap/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
