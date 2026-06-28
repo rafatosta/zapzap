75ba3b0a236a504230a454484e5ef4652529d784
Rafael Tosta <rafa.ecomp@gmail.com>
2026-06-28 16:31:29 -0300

    Update build.sh

--- .github/packaging/snap/build.sh
+++ .github/packaging/snap/build.sh
@@ -2,19 +2,9 @@ set -euo pipefail
 
 rm -rf .snapcraft-project
 mkdir -p .snapcraft-project
-
 rsync -a \
   --exclude .git \
   --exclude .snapcraft-project \
   ./ \
   .snapcraft-project/
-
-if [[ ! -f .github/packaging/snap/snapcraft.yaml ]]; then
-  echo "snapcraft.yaml not found at .github/packaging/snap/snapcraft.yaml"
-  exit 1
-fi
-
 cp .github/packaging/snap/snapcraft.yaml .snapcraft-project/snapcraft.yaml
-
-cd .snapcraft-project
-snapcraft
