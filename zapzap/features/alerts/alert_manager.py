1f8fc3df292d306886dd2872806415a35028ed08
Rafael Tosta <rafa.ecomp@gmail.com>
2026-07-15 20:56:03 -0300

    Wrap AlertManager message text

--- zapzap/features/alerts/alert_manager.py
+++ zapzap/features/alerts/alert_manager.py
@@ -1,39 +1,100 @@
-from PyQt6.QtWidgets import QMessageBox
+from gettext import gettext as _
+from typing import Optional
+
+from PyQt6.QtWidgets import QLabel, QMessageBox, QSizePolicy
 
 from zapzap import LIMITE_USERS
-from gettext import gettext as _
-
 from zapzap.core.config.settings_manager import SettingsManager
 
 
 class AlertManager:
-    @staticmethod
-    def information(parent, title: str, message: str):
-        QMessageBox.information(parent, title, message)
+    MESSAGE_MIN_WIDTH = 320
+    MESSAGE_MAX_WIDTH = 560
 
-    @staticmethod
-    def warning(parent, title: str, message: str):
-        QMessageBox.warning(parent, title, message)
+    @classmethod
+    def _message_box(
+        cls,
+        parent,
+        title: str,
+        message: str,
+        icon: QMessageBox.Icon,
+        buttons: QMessageBox.StandardButton = QMessageBox.StandardButton.Ok,
+        default_button: Optional[QMessageBox.StandardButton] = None,
+    ) -> QMessageBox:
+        message_box = QMessageBox(parent)
+        message_box.setIcon(icon)
+        message_box.setWindowTitle(title)
+        message_box.setText(message)
+        message_box.setStandardButtons(buttons)
 
-    @staticmethod
-    def critical(parent, title: str, message: str):
-        QMessageBox.critical(parent, title, message)
+        if default_button is not None:
+            message_box.setDefaultButton(default_button)
 
-    @staticmethod
-    def question(parent, title: str, message: str) -> bool:
-        response = QMessageBox.question(
+        cls._enable_message_wrapping(message_box)
+        return message_box
+
+    @classmethod
+    def _enable_message_wrapping(cls, message_box: QMessageBox):
+        message_box.setMinimumWidth(cls.MESSAGE_MIN_WIDTH)
+        for label in message_box.findChildren(QLabel):
+            label.setWordWrap(True)
+            label.setMinimumWidth(cls.MESSAGE_MIN_WIDTH)
+            label.setMaximumWidth(cls.MESSAGE_MAX_WIDTH)
+            label.setSizePolicy(
+                QSizePolicy.Policy.Expanding,
+                QSizePolicy.Policy.Preferred,
+            )
+
+    @classmethod
+    def information(cls, parent, title: str, message: str):
+        cls._message_box(
             parent,
             title,
             message,
-            QMessageBox.StandardButton.Yes | QMessageBox.StandardButton.No
-        )
+            QMessageBox.Icon.Information,
+        ).exec()
+
+    @classmethod
+    def warning(cls, parent, title: str, message: str):
+        cls._message_box(
+            parent,
+            title,
+            message,
+            QMessageBox.Icon.Warning,
+        ).exec()
+
+    @classmethod
+    def critical(cls, parent, title: str, message: str):
+        cls._message_box(
+            parent,
+            title,
+            message,
+            QMessageBox.Icon.Critical,
+        ).exec()
+
+    @classmethod
+    def question(cls, parent, title: str, message: str) -> bool:
+        response = cls._message_box(
+            parent,
+            title,
+            message,
+            QMessageBox.Icon.Question,
+            QMessageBox.StandardButton.Yes | QMessageBox.StandardButton.No,
+            QMessageBox.StandardButton.No,
+        ).exec()
         return response == QMessageBox.StandardButton.Yes
 
-    @staticmethod
-    def limit_users(parent):
-        QMessageBox.information(
-            parent, _("Information"), _("Limit of {} users reached.\nIt is not possible to create more users!").format(SettingsManager.get("users/size", LIMITE_USERS)))
+    @classmethod
+    def limit_users(cls, parent):
+        cls.information(
+            parent,
+            _("Information"),
+            _(
+                "Limit of {} users reached.\n"
+                "It is not possible to create more users!"
+            ).format(SettingsManager.get("users/size", LIMITE_USERS)),
+        )
 
-    def no_active_account(parent):
-        QMessageBox.information(
-            parent, _("Attention"), _("No active account!"))
+    @classmethod
+    def no_active_account(cls, parent):
+        cls.information(parent, _("Attention"), _("No active account!"))
