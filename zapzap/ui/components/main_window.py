7ec915f2360e32d85c3335bf09c6d8655a47f511
afcihangir <afcihangir@gmail.com>
2026-09-25 09:17:22 +0300

    feat(ui): add mute buttons beside downloads

--- zapzap/ui/components/main_window.py
+++ zapzap/ui/components/main_window.py
@@ -55,7 +55,21 @@ class MainWindowView(QMainWindow):
         self.menuHelp = QMenu(self.menubar)
         self.menuHelp.setObjectName("menuHelp")
 
-        self.btn_menubar_downloads = QToolButton(self.menubar)
+        self.menubar_corner = QWidget(self.menubar)
+        self.menubar_corner_layout = QHBoxLayout(self.menubar_corner)
+        self.menubar_corner_layout.setContentsMargins(0, 0, 0, 0)
+        self.menubar_corner_layout.setSpacing(5)
+
+        self.btn_menubar_mute = QToolButton(self.menubar_corner)
+        self.btn_menubar_mute.setObjectName("btn_menubar_mute")
+        self.btn_menubar_mute.setAutoRaise(True)
+        self.btn_menubar_mute.setToolButtonStyle(
+            Qt.ToolButtonStyle.ToolButtonIconOnly
+        )
+        self.btn_menubar_mute.setCursor(Qt.CursorShape.PointingHandCursor)
+        self.menubar_corner_layout.addWidget(self.btn_menubar_mute)
+
+        self.btn_menubar_downloads = QToolButton(self.menubar_corner)
         self.btn_menubar_downloads.setObjectName("btn_menubar_downloads")
         self.btn_menubar_downloads.setAutoRaise(True)
         self.btn_menubar_downloads.setToolButtonStyle(
@@ -64,10 +78,13 @@ class MainWindowView(QMainWindow):
         self.btn_menubar_downloads.setCursor(
             Qt.CursorShape.PointingHandCursor
         )
+        self.btn_menubar_mute.setToolTip(_("Mute"))
+        self.btn_menubar_mute.setAccessibleName(_("Mute"))
         self.btn_menubar_downloads.setToolTip(_("Downloads"))
         self.btn_menubar_downloads.setAccessibleName(_("Downloads"))
+        self.menubar_corner_layout.addWidget(self.btn_menubar_downloads)
         self.menubar.setCornerWidget(
-            self.btn_menubar_downloads,
+            self.menubar_corner,
             Qt.Corner.TopRightCorner,
         )
 
@@ -155,8 +172,9 @@ class MainWindowView(QMainWindow):
     def sync_menubar_downloads_button_size(self):
         height = max(28, self.menubar.sizeHint().height())
         icon_size = max(24, height - 16)
-        self.btn_menubar_downloads.setFixedSize(QSize(height + 8, height))
-        self.btn_menubar_downloads.setIconSize(QSize(icon_size, icon_size))
+        for button in (self.btn_menubar_mute, self.btn_menubar_downloads):
+            button.setFixedSize(QSize(height + 8, height))
+            button.setIconSize(QSize(icon_size, icon_size))
 
     def retranslate_ui(self):
         self.setWindowTitle(_("ZapZap"))
